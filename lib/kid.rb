d7bd4200fe9f0e236641e45e79dc3b6ba4a086b2
Nabil Hayet <nabil.bracu@gmail.com>
2020-04-25 23:20:14 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,6 +3,10 @@ require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 
 class Kid
+  attr_accessor :name 
+  def initialize(name)
+    @name =name 
+  end
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
 end
