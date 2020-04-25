b69a194b16d495e47d9ee87afcd91d5af4f714e2
Nabil Hayet <nabil.bracu@gmail.com>
2020-04-25 23:19:29 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,6 +3,9 @@ require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
 class Dancer
+  def initialize(name)
+    @name =name 
+  end 
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
 end
